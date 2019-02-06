67fa558ef6816668486b39bd9b0231e75e54cf90
Wayne Dyck <dyckw@wsdot.wa.gov>
2019-02-05 16:36:32 -0800

    Initial commit of Intranet Classic (2007) theme

---
+++ docroot/sites/wsdot.intranet.dev/themes/custom/intranet_classic/js/ui.js
@@ -0,0 +1,22 @@
+﻿jQuery(document).ready(function() {
+    var loc = window.location.pathname.toLowerCase().match(/(\w+)/i);
+    switch(loc[0]) {
+    case "onedot":
+        jQuery('#mainnav #onedot').addClass('active');
+        break;
+    case "projects":
+        jQuery('#mainnav #projects').addClass('active');
+        break;
+    case "siteindex":
+        jQuery('#mainnav #siteindex').addClass('active');
+        break;
+    case "employee":
+        jQuery('#mainnav #employee').addClass('active');
+        break;
+    case "data":
+        jQuery('#mainnav #data').addClass('active');
+        break;
+    }
+});
+
+
