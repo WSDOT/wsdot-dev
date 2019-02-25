8c4fbc4ed17069b6fbbd425de0ca5ac869dcb9c3
Wayne Dyck <dyckw@wsdot.wa.gov>
2019-02-25 08:59:16 -0800

    DevDesktop multisite export. Cloud db settings block added to settings.php

--- docroot/sites/wsdot.intranet/settings.php
+++ docroot/sites/wsdot.intranet/settings.php
@@ -604,3 +604,7 @@ if (isset($_SERVER['DEVDESKTOP_DRUPAL_SETTINGS_DIR']) && file_exists($_SERVER['D
   require $_SERVER['DEVDESKTOP_DRUPAL_SETTINGS_DIR'] . '/cld_prod_wsdot_dev_wsdot_intranet.inc';
 }
 // </DDSETTINGS>
+
+if (file_exists('/var/www/site-php')) {
+    require('/var/www/site-php/wsdot/wsdot_intranet-settings.inc');
+}
